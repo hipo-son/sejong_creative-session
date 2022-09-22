ef07cf31b2848467081fcbf6d7a263857b369634
Hipo_son <hiposon214@gmail.com>
2022-09-22 13:25:33 +0900

    fix

--- 3.practice/projectile-motion-2D_son.py
+++ 3.practice/projectile-motion-2D_son.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import rc
-from matplotlib.animation import FuncAnimation_
+from matplotlib.animation import FuncAnimation
 
 def Energy(h, v):
   E = g * h + (1/2) * (v**2)
@@ -41,23 +41,3 @@ def update(frame):
 
 ani = FuncAnimation(fig, update, frames=range(0, len(px), 5), init_func = init, blit = False)
 ani
-
-def Energy(h, v):
-  E = g * h + (1/2) * (v**2)
-  return E
-
-g = 9.8; v0 = 10.; theta = 60 * (np.pi/180.); dt = 0.01;
-vx = v0 * np.cos(theta); vy= v0 * np.sin(theta)
-x, y = 0., 0.
-px, py = [], []
-
-while Energy(y, vy) > 0.1:
-  if y < 0 and vy < 0:
-    vy = 0.8 * abs(vy)
-  x += vx * dt
-  y += vy * dt
-  vy -= g * dt
-  px.append(x)
-  py.append(y)
-
-plt.plot(px, py)
